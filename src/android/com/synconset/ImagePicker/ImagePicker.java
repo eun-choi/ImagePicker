88c046080a35fff695d6b570e1bbef892fdda09f
eun-choi <54615424+eun-choi@users.noreply.github.com>
2023-09-21 12:21:59 +0900

    fix android 13 & plugin

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -20,8 +20,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class ImagePicker extends CordovaPlugin {
 
@@ -102,10 +102,15 @@ public class ImagePicker extends CordovaPlugin {
         return false;
     }
 
+    private String getMediaPermissionName() {
+        return Build.VERSION.SDK_INT >= 33 ? Manifest.permission.READ_MEDIA_IMAGES : Manifest.permission.READ_EXTERNAL_STORAGE;
+    }
+
     @SuppressLint("InlinedApi")
     private boolean hasReadPermission() {
         return Build.VERSION.SDK_INT < 23 ||
-            PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this.cordova.getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this.cordova.getActivity(), this.getMediaPermissionName());
+        // PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this.cordova.getActivity(), Manifest.permission.READ_MEDIA_IMAGES);
     }
 
     @SuppressLint("InlinedApi")
@@ -113,8 +118,9 @@ public class ImagePicker extends CordovaPlugin {
         if (!hasReadPermission()) {
             ActivityCompat.requestPermissions(
                 this.cordova.getActivity(),
-                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                new String[] {this.getMediaPermissionName()},
                 PERMISSION_REQUEST_CODE);
+                // new String[] {Manifest.permission.READ_MEDIA_IMAGES},
         }
         // This method executes async and we seem to have no known way to receive the result
         // (that's why these methods were later added to Cordova), so simply returning ok now.
